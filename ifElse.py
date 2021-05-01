f978ea843eb77bbc12487fb43df2e9d219740fc1
janarbek <janarbek11.jk@gmail.com>
2021-05-01 20:37:07 +0600

    мои задания по условным конструкциям

---
+++ ifElse.py
@@ -0,0 +1,17 @@
+# Создайте 2 переменные со значениями 2**3 и 3**2 и покажите значение переменной в которой значение больше.
+
+a = 2**3
+b = 3**2
+print("a=", a)
+print("b=", b)
+
+if a < b:
+    print("a bolshe")
+elif a > b:
+    print("b bolshe")
+
+if a > b:
+    print("a bolshe")
+elif a < b:
+    print("b bolshe")
+
